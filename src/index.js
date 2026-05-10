54a1375cded7cc2d92cbb639fb44f6cf35639c89
raonegone-max <aryangupta.in@gmail.com>
2026-05-10 23:18:35 +0530

    utils made

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
     path:"./.env"
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection error",err); 
+}) 
